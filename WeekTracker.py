f313198b6b6f9371ae20f8365c7e20f88594634a
David <djorgepbl@gmail.com>
2017-03-15 19:55:38 +0000

    full functionality

--- WeekTracker.py
+++ WeekTracker.py
@@ -9,9 +9,7 @@ class WeekTracker:
                 1 : DayTracker(),
                 2 : DayTracker(),
                 3 : DayTracker(),
-                4 : DayTracker(),    
-                5 : DayTracker(),                    
-                6 : DayTracker(),    
+                4 : DayTracker()                
                 }
         self.weekTotalTime = 0
         self.lastDayProcessed = -1
@@ -29,7 +27,7 @@ class WeekTracker:
         datetimeOut = datetime.datetime.strptime(clockedOut, '%d/%m/%y %H:%M')
         if dayOfTheWeek >= 0 and dayOfTheWeek < 7:
             self.weekDays[dayOfTheWeek].add(clockedIn, clockedOut, dayOfTheWeek, duration, tags, comments)
-            self.weekTotalTime += self.weekDays[dayOfTheWeek].currentDuration  
+            self.weekTotalTime += self.weekDays[dayOfTheWeek].currentDuration
             
         '''if self.lastDayProcessed == -1:
             self.lastDayProcessed = dayOfTheWeek
@@ -39,13 +37,16 @@ class WeekTracker:
         '''
             
     def weekTotals(self):
+        '''
+        Calculates week Total Worked Time and remaining time 
+        '''
         weekDuration = 40
         weekTotalDuration = 0
         weekTotalRemaining = 0
         for idx, day in self.weekDays.iteritems():
-            if idx not in (5, 6):
-                 weekTotalDuration += day.dayTotalTimeRounded
-                 weekTotalRemaining += day.remainingHours                 
+            #if idx not in (5,6):
+            weekTotalDuration += day.dayTotalTimeRounded
+            weekTotalRemaining += day.remainingHours                 
         print("Week Totals\n TotalDuration: {} TotalRemaining:{} ".format(weekTotalDuration, weekTotalRemaining))        
         
     def print_data(self):   
@@ -53,15 +54,21 @@ class WeekTracker:
          #   print "%d, %f" %(idx, day.dayTotalTime)
         print (self.weekTotalTime)
         for idx, day in self.weekDays.iteritems():
-            if idx not in (5, 6):
-                print("idx: {} total time:{} remaining {} ".format(idx,day.dayTotalTime, day.remainingHours))
-                if self.weekDays[idx].clokOutChanged == True:                    
-                    print("with offset {} new clockOut {}".format(self.weekDays[idx].deltaAdded,self.weekDays[idx].lastDateTimeOut + self.weekDays[idx].deltaAdded))
-                    print("with offset {} new clockOut {}".format(self.weekDays[idx].deltaAdded,self.weekDays[idx].calcCheckOut))
+            #if idx not in (5, 6):            
+            if self.weekDays[idx].clokOutChanged == True:                    
+                print("last clockout {} with offset {} new clockOut {}".format(self.weekDays[idx].lastDateTimeOut, self.weekDays[idx].deltaAdded,self.weekDays[idx].lastDateTimeOut + self.weekDays[idx].deltaAdded))
+                if self.weekDays[idx].newDatetimeIn != 0:
+                    print(" newDatetimeIn {} with offset {} new clockOut {}".format(self.weekDays[idx].newDatetimeIn,self.weekDays[idx].deltaAdded,self.weekDays[idx].calcCheckOut))
+            if self.weekDays[idx].calcXCheckOut != 0:
+                print(" calcXCheckin {} with lunch duration {} and worktime {} => checkOut at {} ".format(self.weekDays[idx].calcXCheckin,self.weekDays[idx].calcXLunchDuration,self.weekDays[idx].calcXWorkTimeTarget,self.weekDays[idx].calcXCheckOut))
+            print("idx: {} total time:{} remaining {} ".format(idx,day.dayTotalTime, day.remainingHours))
                     
         self.weekTotals()
 
-    def calcFor40Hours(self):
+    def calcFor40Hours(self, clockIn0, clockIn1, clockIn2, clockIn3, clockIn4, lunchTimeHour, lunchTimeMinute):
         #if day < 8 hors, calc day to get 8 hour
-        pass
-        
+        self.weekDays[0].calcForXHourMinute(clockIn0, lunchTimeHour, lunchTimeMinute, 8, 00)        
+        self.weekDays[1].calcForXHourMinute(clockIn1, lunchTimeHour, lunchTimeMinute, 8, 00)        
+        self.weekDays[2].calcForXHourMinute(clockIn2, lunchTimeHour, lunchTimeMinute, 8, 00)        
+        self.weekDays[3].calcForXHourMinute(clockIn3, lunchTimeHour, lunchTimeMinute, 8, 00)        
+        self.weekDays[4].calcForXHourMinute(clockIn4, lunchTimeHour, lunchTimeMinute, 8, 00)        
