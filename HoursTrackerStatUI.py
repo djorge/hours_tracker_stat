8dbaa33961d87702e077b41c32de4db3fba60105
David <djorgepbl@gmail.com>
2017-04-02 00:38:43 +0100

    initial

---
+++ HoursTrackerStatUI.py
@@ -0,0 +1,13 @@
+#! python2
+# coding: utf-8
+import ui, time
+
+#HoursTrackerStat (Main)
+#WeekTracker week
+import csv
+import appex
+import codecs
+
+
+v = ui.load_view('HoursTrackerStatUI')
+v.present()
